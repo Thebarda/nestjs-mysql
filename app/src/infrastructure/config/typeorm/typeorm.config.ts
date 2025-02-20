5e5a2830495b14a647b63db9657d4eaa40cea4a6
Tom Darneix <tomdar87@outlook.com>
2025-02-20 20:02:45 +0100

    stuff

--- app/src/infrastructure/config/typeorm/typeorm.config.ts
+++ app/src/infrastructure/config/typeorm/typeorm.config.ts
@@ -1,14 +1,14 @@
 import type { DataSourceOptions } from "typeorm";
 
 const config: DataSourceOptions = {
-  type: "mysql",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: [`${__dirname}./../../**/*.entity{.ts,.js}`],
-  synchronize: false,
+	type: "mysql",
+	host: process.env.DB_HOST,
+	port: Number(process.env.DB_PORT),
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	entities: [`${__dirname}./../../**/*.entity{.ts,.js}`],
+	synchronize: false,
 };
 
 export default config;
